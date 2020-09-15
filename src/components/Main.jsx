df629f23f90c9e352b8b2003788c6a3046d4ee35
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 11:51:22 +0100

    Creates Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Map from "./Map.jsx";
+
+const Main = () => {
+  return (
+    <section>
+      <Header />
+      <Map />
+    </section>
+  );
+};
+
+export default Main;
