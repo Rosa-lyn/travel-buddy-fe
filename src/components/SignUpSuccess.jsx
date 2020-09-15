fc0f07e5e3f3aa8b133763f860aaaaf8020b8c9d
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 12:09:55 +0100

    Creates SignUpSuccess component

---
+++ src/components/SignUpSuccess.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUpSuccess = () => {
+  return (
+    <div>
+      <p>Sign up successful!</p>
+      <p>Welcome in!</p>
+      <button>login!</button>
+    </div>
+  );
+};
+
+export default SignUpSuccess;
