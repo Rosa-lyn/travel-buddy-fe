3f3de4de0b98fc6f6df559a87ee503d9ee537df1
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 11:18:54 +0100

    Creates SignUp component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <section>
+      <h1>sign up</h1>
+      <form action=""></form>
+    </section>
+  );
+};
+
+export default SignUp;
