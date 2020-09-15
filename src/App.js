ffcba9ecbbe3536abe0b8e27a9ebbb7d653ec7e9
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 09:49:58 +0100

    Adds Header and Map components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Map from "./components/Map.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Map />
+    </div>
+  );
 }
 
 export default App;
