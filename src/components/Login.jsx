7d2345c461ee4f1a8e8fc47979d1cec824233b6d
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 11:18:25 +0100

    Renames Login component to LoginButton

--- src/components/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <button>login</button>;
-};
-
-export default Login;
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+
+const LoginButton = () => {
+  return (
+    <section>
+      {/*  opens login page */}
+      <button>login</button>
+      <LoginForm />
+    </section>
+  );
+};
+
+export default LoginButton;
