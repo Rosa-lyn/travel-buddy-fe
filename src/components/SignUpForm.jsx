d96ea53dcb2e2c70cdc1e7c4695d54f91c196786
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 12:09:40 +0100

    Adds form fields and @reach-router navigate function

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { navigate } from "@reach/router";
 
-const SignUpForm = () => {
-  return (
-    <section>
-      <h1>sign up</h1>
-      <form action=""></form>
-    </section>
-  );
-};
+class SignUpForm extends Component {
+  handleSubmit = () => {
+    navigate("/signupsuccess");
+  };
+  render() {
+    return (
+      <section>
+        <h1>sign up</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" />
+          <input type="submit" />
+        </form>
+      </section>
+    );
+  }
+}
 
 export default SignUpForm;
