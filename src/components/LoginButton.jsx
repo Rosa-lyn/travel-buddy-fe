7d5cdff7632592a4d3123d84cd0973d5e0040d9d
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 11:47:19 +0100

    Removes LoginForm component

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import LoginForm from "./LoginForm";
 
 const LoginButton = () => {
   return (
     <section>
       {/*  opens login page */}
       <button>login</button>
-      <LoginForm />
     </section>
   );
 };
