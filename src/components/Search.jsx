4dbd731e37115e671eab91a345d13fcf028483d8
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 10:03:03 +0100

    Creates Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <form action="">
+      <input type="text" value="search..." />
+      <input type="submit" value="search" />
+    </form>
+  );
+};
+
+export default Search;
