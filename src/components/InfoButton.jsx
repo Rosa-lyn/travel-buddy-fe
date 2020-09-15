6864f789eb532ad15be64ddc9fb14f5d065a4390
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 11:17:36 +0100

    creates InfoButton component

---
+++ src/components/InfoButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InfoPopup from "./InfoPopup";
+
+const InfoButton = () => {
+  return (
+    <section>
+      {/*  opens info popup */}
+      <button>i</button>
+      <InfoPopup />
+    </section>
+  );
+};
+
+export default InfoButton;
