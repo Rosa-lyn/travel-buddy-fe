134738ad38151a704302f6ab942bdb1074f9d9af
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 18:10:20 +0100

    Adds example experience to state and marker

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -6,6 +6,19 @@ class MapContainer extends Component {
   state = {
     center: [0, 0],
     zoom: 2,
+    experiences: [
+      {
+        title: "Fab place to watch street performers.",
+        body:
+          "Saw some great acts here on saturday lunchtime, look out for the guy with the pogo stick, he will blow your mind.",
+        username: "joey_traveller",
+        created_at: 1560347936852,
+        location_lat: 53.96268,
+        location_long: -1.085605,
+        likes: 13,
+        belongs_to_tag_text: ["#streetPerformers", "#outdoorExperience"],
+      },
+    ],
   };
   zoomToExperience = (event) => {
     const { lat, lng } = event.latlng;
@@ -15,24 +28,35 @@ class MapContainer extends Component {
     this.setState({ zoom: 14 });
   };
   render() {
-    const { center, zoom } = this.state;
+    const { center, zoom, experiences } = this.state;
     return (
       <Map className="Map" center={center} zoom={zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker
-          position={[53.5546385, 9.9491622]}
-          onClick={this.zoomToExperience}
-        >
-          <Popup onClose={this.closePopup}>
-            <h1>Incredible busker with pots and pans</h1>
-            <p>John X</p>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
-            <Link to="/experience">...read more</Link>
-          </Popup>
-        </Marker>
+        {experiences.map((experience) => {
+          console.log(experience);
+          const {
+            title,
+            username,
+            location_lat,
+            location_long,
+            created_at,
+          } = experience;
+          const markerPosition = [location_lat, location_long];
+          return (
+            <Marker position={markerPosition} onClick={this.zoomToExperience}>
+              <Popup onClose={this.closePopup}>
+                <h1>{title}</h1>
+                <p>
+                  {username} on {created_at}
+                </p>
+                <Link to="/experience">...read more</Link>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
     );
   }
