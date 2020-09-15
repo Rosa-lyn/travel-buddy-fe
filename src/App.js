74800123dfa058f35c8b3d22014c04188f29eba9
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 12:10:47 +0100

    Adds SignUpSuccess component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Router } from "@reach/router";
 import "./App.css";
 import Main from "./components/Main.jsx";
 import SignUpForm from "./components/SignUpForm.jsx";
+import SignUpSuccess from "./components/SignUpSuccess.jsx";
 import LoginForm from "./components/LoginForm.jsx";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Main path="/" />
         <SignUpForm path="/signup" />
+        <SignUpSuccess path="/signupsuccess" />
         <LoginForm path="/login" />
       </Router>
     </div>
