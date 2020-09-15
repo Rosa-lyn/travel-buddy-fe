20abcb7fc7380221420e842e240337ac61152ba5
Rosalyn Land <roz.s.land@gmail.com>
2020-09-15 17:44:44 +0100

    Adds default center / zoom in state, and adds zoomToExperience / closePopup methods

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -3,15 +3,30 @@ import { Link } from "@reach/router";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 class MapContainer extends Component {
+  state = {
+    center: [0, 0],
+    zoom: 2,
+  };
+  zoomToExperience = (event) => {
+    const { lat, lng } = event.latlng;
+    this.setState({ center: [lat, lng], zoom: 18 });
+  };
+  closePopup = () => {
+    this.setState({ zoom: 14 });
+  };
   render() {
+    const { center, zoom } = this.state;
     return (
-      <Map className="Map" center={[53.5546385, 9.9491622]} zoom={15}>
+      <Map className="Map" center={center} zoom={zoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[53.5546385, 9.9491622]}>
-          <Popup>
+        <Marker
+          position={[53.5546385, 9.9491622]}
+          onClick={this.zoomToExperience}
+        >
+          <Popup onClose={this.closePopup}>
             <h1>Incredible busker with pots and pans</h1>
             <p>John X</p>
             <p>Lorem ipsum dolor sit amet consectetur</p>
