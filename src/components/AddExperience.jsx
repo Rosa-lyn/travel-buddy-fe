a74f0562a88b166260e2af65afb2cbd34470ad66
“Steve <stevedunnad@gmail.com>
2020-09-17 07:51:36 +0100

    Separates onChange functions for title input and body input

--- src/components/AddExperience.jsx
+++ src/components/AddExperience.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormContainer, FormInput, FormTitle, FormTextarea, Button, ButtonContainer, FormLabel, FormInnnerContainer } from '../styles/AddExperienceStyles'
 
 class AddExperience extends React.Component {
 
@@ -8,13 +9,17 @@ class AddExperience extends React.Component {
     body: "",
   }
 
-  handleChange = (e) => {
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.value })
+  }
+
+  handleBodyChange = (e) => {
     this.setState({ body: e.target.value })
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
-    this.setState({ body: "" })
+    this.setState({ body: "", title: "" })
     const { body } = this.state;
     // this.state.body.length && await api.addExperience(this.props.id, body)
     // need separate file with api calls
@@ -23,17 +28,23 @@ class AddExperience extends React.Component {
   render() {
 
     return (
-      <div>
-        <h1>add your experience</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="addExperience">add your own unique experience</label>
-          <textarea onChange={this.handleChange} type="textarea" value={this.state.body}
-            id="addExperience" name="addExperience" placeholder="add your own unique experience"
-            rows="6" cols="40" />
-          <input type="submit" value="add image" />
-          <input type="submit" value="post" />
-        </form>
-      </div>
+      <FormContainer>
+        <FormInnnerContainer>
+          <FormTitle>add your experience</FormTitle>
+          <form onSubmit={this.handleSubmit}>
+            <FormLabel htmlFor="addTitle">add experience title</FormLabel>
+            <FormInput id="1" onChange={this.handleTitleChange} type="text" value={this.state.title} placeholder="add your title"></FormInput>
+            <FormLabel htmlFor="addExperience">describe your experience</FormLabel>
+            <FormTextarea id="2" onChange={this.handleBodyChange} type="textarea" value={this.state.body}
+              id="addExperience" name="addExperience" placeholder="add your experience"
+              rows="6" cols="40" />
+            <ButtonContainer>
+              <Button type="submit" value="add image" />
+              <Button type="submit" value="post" />
+            </ButtonContainer>
+          </form>
+        </FormInnnerContainer>
+      </FormContainer>
     );
   }
 };
