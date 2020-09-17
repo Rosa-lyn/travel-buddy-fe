78613c3589a0fd3c627e586215e2e9299ad16c73
“Steve <stevedunnad@gmail.com>
2020-09-17 16:19:24 +0100

    changes add image button to more appropriate link property

--- src/components/AddExperience.jsx
+++ src/components/AddExperience.jsx
@@ -12,7 +12,8 @@ import {
   ButtonContainer,
   FormLabel,
   FormInnnerContainer,
-  CloseButton
+  CloseButton,
+  AddImageButton
 } from '../styles/AddExperienceStyles'
 
 class AddExperience extends Component {
@@ -57,6 +58,10 @@ class AddExperience extends Component {
         });
   }
 
+  handleClick = () => {
+
+  }
+
   render() {
 
     return (
@@ -68,12 +73,12 @@ class AddExperience extends Component {
             <FormLabel htmlFor="addTitle">add experience title</FormLabel>
             <FormInput onChange={this.handleTitleChange} type="text" value={this.state.title} placeholder="add your title"></FormInput>
             <FormLabel htmlFor="addExperience">describe your experience</FormLabel>
-            <FormTextarea experience_id="2" onChange={this.handleBodyChange} type="textarea" value={this.state.body}
+            <FormTextarea onChange={this.handleBodyChange} type="textarea" value={this.state.body}
               id="addExperience" name="addExperience" placeholder="add your experience"
               rows="6" cols="40" />
             <ButtonContainer>
-              <Button type="submit" value="add image" />
-              <Button type="submit" value="post" />
+              <AddImageButton to="/addimage">add image</AddImageButton>
+              <Button onClick={this.handleSubmit} type="submit" value="post" />
             </ButtonContainer>
           </form>
         </FormInnnerContainer>
