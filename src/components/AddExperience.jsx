3ddd5368cdf3a743ff957b11f723a410183e09b0
“Steve <stevedunnad@gmail.com>
2020-09-17 12:13:19 +0100

    Adds extar input field to post tags

--- src/components/AddExperience.jsx
+++ src/components/AddExperience.jsx
@@ -15,11 +15,12 @@ import {
   CloseButton
 } from '../styles/AddExperienceStyles'
 
-class AddExperience extends React.Component {
+class AddExperience extends Component {
 
   state = {
     title: "",
     body: "",
+    tag: "",
     err: "",
     isLoading: true,
   }
@@ -32,18 +33,23 @@ class AddExperience extends React.Component {
     this.setState({ body: e.target.value })
   }
 
-  handleSubmit = async (e) => {
+  handleTagChange = (e) => {
+    this.setState({ tag: e.target.value })
+  }
+
+  handleSubmit = (e) => {
     e.preventDefault()
     this.setState({ body: "", title: "" })
     const { title } = this.state;
     const { body } = this.state;
+    const { tag } = this.state;
     const { username } = this.props;
     this.state.body &&
       api
-        .postExperience(title, body, username)
+        .postExperience(title, body, tag, username)
         .then((postedExperience) => {
           this.props.addExperienceToState(postedExperience);
-          this.setState({ title: "", body: "" })
+          this.setState({ title: "", body: "", tag: "" })
         })
         .catch((err) => {
           this.setState({ err: err.response.data.msg, isLoading: false });
@@ -63,11 +69,13 @@ class AddExperience extends React.Component {
           <CloseButton><Link to="/">x</Link></CloseButton>
           <form onSubmit={this.handleSubmit}>
             <FormLabel htmlFor="addTitle">add experience title</FormLabel>
-            <FormInput experience_id="1" onChange={this.handleTitleChange} type="text" value={this.state.title} placeholder="add your title"></FormInput>
+            <FormInput onChange={this.handleTitleChange} type="text" value={this.state.title} placeholder="add your title"></FormInput>
             <FormLabel htmlFor="addExperience">describe your experience</FormLabel>
             <FormTextarea experience_id="2" onChange={this.handleBodyChange} type="textarea" value={this.state.body}
               id="addExperience" name="addExperience" placeholder="add your experience"
               rows="6" cols="40" />
+            <FormLabel htmlFor="addTag">add a custom tag</FormLabel>
+            <FormInput onChange={this.handleTagChange} type="text" value={this.state.tag} placeholder="add your tags"></FormInput>
             <ButtonContainer>
               <Button type="submit" value="add image" />
               <Button type="submit" value="post" />
