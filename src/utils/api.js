b8ed2379cb682d5840a61cede33a68cf7c5a4cfd
helenjanei <helenjanei@gmail.com>
2020-09-21 11:59:21 +0100

    fixes api responce expectations

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,14 +6,13 @@ const instance = axios.create({
 
 export const getAllExperiences = () => {
   const query = {
-    query: "{experiences {experience_id title body username created_at location_lat location_long likes}}",
+    query:
+      "{experiences {experience_id title body username created_at location_lat location_long likes}}",
   };
   return instance.post("/", query).then(
     ({
       data: {
-        data: {
-          experiences
-        },
+        data: { experiences },
       },
     }) => experiences
   );
@@ -44,48 +43,35 @@ export const getSingleExperience = (experience_id) => {
     image_URL
   }}`,
   };
-  return instance.post("/", query).then(({
-    data: {
-      data
-    }
-  }) => data);
+  return instance.post("/", query).then(({ data: { data } }) => data);
 };
 
 //get graphql running and check return keys for then statements
 
-export const postExperience = (title, body, username, created_at, location_lat, location_long) => {
+export const postExperience = (
+  title,
+  body,
+  username,
+  created_at,
+  location_lat,
+  location_long
+) => {
   const mutation = {
     query: `mutation{addExperience(input: {title:${title}, body:${body}, username:${username}, created_at:${created_at}, location_lat:${location_lat}, location_long:${location_long}}) {experience_id title body username created_at location_lat location_long likes}}`,
   };
   return instance
     .post("/", mutation)
-    .then(({
-      data: {
-        data
-      }
-    }) => data.addExperience);
+    .then(({ data: { data } }) => data.addExperience);
   //check
 };
-
 
 export const postComment = (experience_id, body, username) => {
   const mutation = {
     query: `mutation{addComment(input: {experience_id:${experience_id}, body:${body}, username:${username}}) {comment_id body username created_at likes}}`,
   };
-  return instance
-    .post("/", mutation)
-    .then(({
-      data: {
-        data
-      }
-    }) => data);
-
+  return instance.post("/", mutation).then(({ data: { data } }) => data);
 };
 
-
-export const deleteComment = () => {
-  console.log("not written yet");
-};
 export const getCommentsByExperienceId = (experience_id) => {
   const query = {
     query: `{comments(experience_id:${experience_id}) {
@@ -94,30 +80,25 @@ export const getCommentsByExperienceId = (experience_id) => {
         body
         likes
         username
-      }}`
-  }
+      }}`,
+  };
   return instance.post("/", query).then(
     ({
       data: {
-        data: {
-          experiences
-        },
+        data: { experiences },
       },
     }) => experiences
   );
 };
-
 export const deleteComment = (comment_id) => {
   return instance.delete(`/comments/${comment_id}`);
 };
 
-export const patchLikes = (experience_id, image_desc, image_URL) => {
-  // const mutation = {
-  //   query: `mutation{addImage(input:{experience_id:${experience_id}, image_desc:${image_desc}, image_URL:${image_URL})}){image_id image_desc image_URL experience_id}}`,
-  // };
-
-  return instance.patch(`/${type}/${id}`, {
-    inc_likes
-  });
-
+export const patchLikes = (experience_id, inc_likes) => {
+  const mutation = {
+    query: `mutation{updateExperienceLikes(input:{experience_id:${experience_id}, inc_likes: ${inc_likes}}){experience_id title body username created_at location_lat location_long likes}}`,
+  };
+  return instance
+    .patch(`/`, mutation)
+    .then(({ data: { updateExperienceLikes } }) => updateExperienceLikes);
 };
