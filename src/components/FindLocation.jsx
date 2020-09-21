211cda5fe7b5085a2795617e4c57a31cac63d134
“Steve <stevedunnad@gmail.com>
2020-09-21 09:58:40 +0100

    adds compass icon to button

--- src/components/FindLocation.jsx
+++ src/components/FindLocation.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { FindMeButton } from '../styles/headerStyles';
-import { PlusCircleIcon } from '@primer/octicons-react'
 
 const FindLocation = () => {
   return (
     <FindMeButton size={16} >
-      <span role="img" aria-label="compass">
-        <PlusCircleIcon />
-      </span>
+      {/* <span role="img" aria-label="compass"> */}
+      <i class="far fa-compass" />
+      {/* </span> */}
     </FindMeButton>
   );
 };
