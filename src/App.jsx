725d0d9c5f0e4df0de1a48d81be2c7ae664a845c
Rosalyn Land <roz.s.land@gmail.com>
2020-09-21 16:24:39 +0100

    changes App to class component to add loggedInUser to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Router } from "@reach/router";
 import "./App.css";
 import Main from "./components/Main.jsx";
@@ -7,24 +7,32 @@ import SignUpSuccess from "./components/SignUpSuccess.jsx";
 import LoginForm from "./components/LoginForm.jsx";
 import ExperienceScreen from "./components/ExperienceScreen";
 import AddExperience from "./components/AddExperience";
-import AddComment from "./components/AddComment";
 import AddImage from "./components/AddImage";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Main path="/" />
-        <SignUpForm path="/signup" />
-        <SignUpSuccess path="/signupsuccess" />
-        <LoginForm path="/login" />
-        <ExperienceScreen path="/experience/:experience_id" />
-        <AddExperience path="/addexperience" />
-        <AddComment path="/addcomment" />
-        <AddImage path="/addimage" />
-      </Router>{" "}
-    </div>
-  );
+class App extends Component {
+  state = {
+    loggedInUser: "burt1943",
+  };
+  render() {
+    const { loggedInUser } = this.state;
+    return (
+      <div className="App">
+        <Router>
+          <Main path="/" />
+          <SignUpForm path="/signup" />
+          <SignUpSuccess path="/signupsuccess" />
+          <LoginForm path="/login" />
+          <ExperienceScreen
+            path="/experience/:experience_id"
+            loggedInUser={loggedInUser}
+          />
+          <AddExperience path="/addexperience" />
+          {/* <AddComment path="/addcomment" /> */}
+          <AddImage path="/addimage" />
+        </Router>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
