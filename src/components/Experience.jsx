a5b5773f37a5da7554de94bd1d56841f3fad6a37
“Steve <stevedunnad@gmail.com>
2020-09-23 16:33:32 +0100

    adds styling for experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import LikeHandler from "./LikeHandler.jsx";
+import '../styles/style.css';
 
 const Experience = ({ experience, images }) => {
   const { title, body, username, created_at, likes } = experience;
   const date = new Date(+created_at);
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{body}</p>
-      {images.map((image) => (
-        <img
-          className="experience-image"
-          src={image.image_URL}
-          alt={image.image_desc}
-          key={image.image_id}
-        ></img>
-      ))}
-      <p>
-        {username} on {date.toLocaleString()}
-      </p>
-      <p>
-        <LikeHandler />
+    <div className="outer-container">
+      <div className="inner-container">
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <div className="image-container">
+          {images.map((image) => (
+            <img
+              className="experience-image"
+              src={image.image_URL}
+              alt={image.image_desc}
+              key={image.image_id}
+            ></img>
+          ))}
+        </div>
+        <p>
+          {username} on {date.toLocaleString()}
+        </p>
+        <p>
+          <LikeHandler />
         likes: {likes}
-      </p>
+        </p>
+      </div>
     </div>
+
   );
 };
 
