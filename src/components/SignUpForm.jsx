18913acf56c37ead44e214f143620465e4688304
helenjanei <helenjanei@gmail.com>
2020-09-24 12:42:04 +0100

    Styles SignUpForm

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import { navigate } from "@reach/router";
-import { SignUpTitle } from '../styles/headerStyles';
+
+import {
+  FormContainer,
+  FormInput,
+  FormTitle,
+  FormTextarea,
+  Button,
+  ButtonContainer,
+  FormLabel,
+  FormInnnerContainer,
+  CloseButton,
+  FormFont,
+  LogoContainer,
+  LogoType,
+} from "../styles/SignUpFormStyles.js";
 
 class SignUpForm extends Component {
   handleSubmit = () => {
@@ -9,12 +23,35 @@ class SignUpForm extends Component {
   render() {
     return (
       <section>
-        <SignUpTitle>sign up</SignUpTitle>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" />
-          <input type="submit" />
-        </form>
+        <LogoContainer>
+          <LogoType>travel buddy</LogoType>
+        </LogoContainer>
+        <FormContainer>
+          {/* div */}
+          <FormInnnerContainer>
+            {/* div */}
+            <CloseButton to="/">x</CloseButton>
+            <FormTitle>Sign Up</FormTitle>
+            <p>It's quick and easy.</p>
+            <FormFont>
+              {/* form */}
+              <FormInput type="text" placeholder="First name"></FormInput>
+              <FormInput type="text" placeholder="Surname"></FormInput>
+              <FormInput
+                type="text"
+                placeholder="Mobile number or email address"
+              ></FormInput>
+              <FormLabel htmlFor="password">
+                Create Password (must be at least 8 characters):
+              </FormLabel>
+              <FormInput type="password" placeholder="password" />
+
+              <ButtonContainer>
+                <Button type="submit" value="login" />
+              </ButtonContainer>
+            </FormFont>
+          </FormInnnerContainer>
+        </FormContainer>
       </section>
     );
   }
