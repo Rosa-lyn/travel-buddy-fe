8dcb4b6c5e76f4d6575444ecf7b4448a5730e2a8
“Steve <stevedunnad@gmail.com>
2020-09-25 13:56:06 +0100

    deletes obsolete code from experience map

--- src/components/ExperienceMap.jsx
+++ src/components/ExperienceMap.jsx
@@ -40,65 +40,68 @@ const ExperienceMap = (props) => {
           toggle={toggle}
         />
       ) : (
-        <Map
-          className="Map"
-          center={center}
-          zoom={zoom}
-          onclick={addExperience}
-        >
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-          />
-          {experiences.map((experience) => {
-            const {
-              experience_id,
-              title,
-              username,
-              location_lat,
-              location_long,
-              created_at,
-            } = experience;
-            const markerPosition = [location_lat, location_long];
-            return (
+
+          <Map
+            className="Map"
+            center={center}
+            zoom={zoom}
+            onclick={addExperience}
+          >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+            />
+            {experiences.map((experience) => {
+              const {
+                experience_id,
+                title,
+                username,
+                location_lat,
+                location_long,
+                created_at,
+              } = experience;
+              const markerPosition = [location_lat, location_long];
+              return (
+                <Marker
+                  key={experience_id}
+                  position={markerPosition}
+                  icon={myIcon}
+                  className="marker"
+                >
+                  <Popup
+                    onClose={closePopup}
+                    className="custom-popup"
+                  >
+                    <h1>{title}</h1>
+                    <p>
+                      {username} on {created_at}
+                    </p>
+                    <Link to={`/experience/${experience_id}`}>...read more</Link>
+                  </Popup>
+                </Marker>
+              );
+            })}
+            {/* this marker will only show when the map is clicked */}
+            {newExperience !== null && (
               <Marker
-                key={experience_id}
-                position={markerPosition}
+                key={newExperience.experience_id}
+                position={[
+                  newExperience.location_lat,
+                  newExperience.location_long,
+                ]}
+                onClick={zoomToExperience}
                 icon={myIcon}
                 className="marker"
+                ref={showMarker}
               >
-                <Popup onClose={closePopup} className="custom-popup">
-                  <h1>{title}</h1>
-                  <p>
-                    {username} on {created_at}
-                  </p>
-                  <Link to={`/experience/${experience_id}`}>...read more</Link>
+                <Popup className="custom-popup" onClose={deleteExperience}>
+                  <p>Do you want to add your experience here?</p>
+                  <button onClick={toggle}>yes</button>
                 </Popup>
               </Marker>
-            );
-          })}
-
-          {/* this marker will only show when the map is clicked */}
-          {newExperience !== null && (
-            <Marker
-              key={newExperience.experience_id}
-              position={[
-                newExperience.location_lat,
-                newExperience.location_long,
-              ]}
-              onClick={zoomToExperience}
-              icon={myIcon}
-              className="marker"
-              ref={showMarker}
-            >
-              <Popup className="custom-popup" onClose={deleteExperience}>
-                <p>Do you want to add your experience here?</p>
-                <button onClick={toggle}>yes</button>
-              </Popup>
-            </Marker>
-          )}
-        </Map>
-      )}
+            )}
+          </Map>
+        )}
     </>
   );
 };
