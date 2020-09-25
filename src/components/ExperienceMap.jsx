15ea1ca5538f7bc878685d9b486a65cd6c9b7692
Rosalyn Land <roz.s.land@gmail.com>
2020-09-25 15:09:21 +0100

    formats date on experience map popup

--- src/components/ExperienceMap.jsx
+++ src/components/ExperienceMap.jsx
@@ -41,68 +41,65 @@ const ExperienceMap = (props) => {
           toggleMapClicked={toggleMapClicked}
         />
       ) : (
-
-          <Map
-            className="Map"
-            center={center}
-            zoom={zoom}
-            onclick={addExperience}
-          >
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-            />
-            {experiences.map((experience) => {
-              const {
-                experience_id,
-                title,
-                username,
-                location_lat,
-                location_long,
-                created_at,
-              } = experience;
-              const markerPosition = [location_lat, location_long];
-              return (
-                <Marker
-                  key={experience_id}
-                  position={markerPosition}
-                  icon={myIcon}
-                  className="marker"
-                >
-                  <Popup
-                    onClose={closePopup}
-                    className="custom-popup"
-                  >
-                    <h1>{title}</h1>
-                    <p>
-                      {username} on {created_at}
-                    </p>
-                    <Link to={`/experience/${experience_id}`}>...read more</Link>
-                  </Popup>
-                </Marker>
-              );
-            })}
-            {/* this marker will only show when the map is clicked */}
-            {newExperience !== null && (
+        <Map
+          className="Map"
+          center={center}
+          zoom={zoom}
+          onclick={addExperience}
+        >
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          />
+          {experiences.map((experience) => {
+            const {
+              experience_id,
+              title,
+              username,
+              location_lat,
+              location_long,
+              created_at,
+            } = experience;
+            const markerPosition = [location_lat, location_long];
+            return (
               <Marker
-                key={newExperience.experience_id}
-                position={[
-                  newExperience.location_lat,
-                  newExperience.location_long,
-                ]}
-                onClick={zoomToExperience}
+                key={experience_id}
+                position={markerPosition}
                 icon={myIcon}
                 className="marker"
-                ref={showMarker}
               >
-                <Popup className="custom-popup" onClose={deleteExperience}>
-                  <p>Do you want to add your experience here?</p>
-                  <button onClick={toggleMapClicked}>yes</button>
+                <Popup onClose={closePopup} className="custom-popup">
+                  <h1>{title}</h1>
+                  <p>
+                    {username} on{" "}
+                    {new Date(+created_at).toLocaleString().slice(0, -10)}
+                  </p>
+                  <Link to={`/experience/${experience_id}`}>...read more</Link>
                 </Popup>
               </Marker>
-            )}
-          </Map>
-        )}
+            );
+          })}
+          {/* this marker will only show when the map is clicked */}
+          {newExperience !== null && (
+            <Marker
+              key={newExperience.experience_id}
+              position={[
+                newExperience.location_lat,
+                newExperience.location_long,
+              ]}
+              onClick={zoomToExperience}
+              icon={myIcon}
+              className="marker"
+              ref={showMarker}
+            >
+              <Popup className="custom-popup" onClose={deleteExperience}>
+                <p>Do you want to add your experience here?</p>
+                <button onClick={toggleMapClicked}>yes</button>
+              </Popup>
+            </Marker>
+          )}
+        </Map>
+      )}
     </>
   );
 };
