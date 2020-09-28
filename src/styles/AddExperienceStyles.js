ff4cbb4d719c006fe2abd3bfd7ab5e9c0c947eb7
Rosalyn Land <roz.s.land@gmail.com>
2020-09-28 09:16:00 +0100

    fixes placeholder error and changes div to form

--- src/styles/AddExperienceStyles.js
+++ src/styles/AddExperienceStyles.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { Link } from "@reach/router";
-import {
-  device
-} from "./@MediaRules.js"
+import { device } from "./@MediaRules.js";
 
-export const OuterFormContainer = styled.div `
+export const OuterFormContainer = styled.div`
   width: 100vw;
   margin: 0;
   position: relative;
@@ -19,18 +17,18 @@ export const FormContainer = styled.div`
   flex-direction: column;
   width: 90vw;
   border: rgb(248, 166, 248) solid 1px;
-  border-radius: .3rem;
+  border-radius: 0.3rem;
   margin: 0;
   position: relative;
   top: 50%;
   left: 50%;
   transform: translate(-50%, 0);
 
-  @media ${device.tablet} { 
+  @media ${device.tablet} {
     max-width: 530px;
   }
 
-  @media ${device.laptop} { 
+  @media ${device.laptop} {
     max-width: 600px;
   }
 
@@ -47,11 +45,11 @@ export const FormInnnerContainer = styled.div`
   border: none;
   margin: auto;
 
-@media ${device.tablet} { 
+  @media ${device.tablet} {
     max-width: 500px;
   }
 
-  @media ${device.laptop} { 
+  @media ${device.laptop} {
     max-width: 565px;
   }
 
@@ -64,7 +62,7 @@ export const FormLabel = styled.label`
   display: block;
   text-align: left;
   font-family: helvetica;
-  font-size: .7rem;
+  font-size: 0.7rem;
   font-weight: 200;
   color: rgb(63, 63, 63);
   margin: 15px 0 0 0;
@@ -78,39 +76,39 @@ export const FormTitle = styled.h1`
   margin: 0;
 `;
 
-export const FormFont = styled.div`
+export const FormFont = styled.form`
   font-family: Arial, Helvetica, sans-serif;
 `;
 
 export const FormInput = styled.input`
-background-color: #C5C5C5;
+  background-color: #c5c5c5;
   height: 1.5rem;
   width: 79vw;
   border-style: none;
-  border-radius: .3rem;
+  border-radius: 0.3rem;
   margin: 9px 5px 5px 0;
 
-::placeholder,
-::-webkit-input-placeholder {
-  font-family: inherit;
-  font-size: .8rem;
-  font-weight: 200;
-  color: #002352;
-  padding: 0 0 0 .2rem;
-}
-:-ms-input-placeholder {
-  font-family: inherit;
-  font-size: .8rem;
-  font-weight: 200;
-  color: #002352;
-  padding: 0 0 0 .2rem;
-}
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    font-family: inherit;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: #002352;
+    padding: 0 0 0 0.2rem;
+  }
+  :-ms-input-placeholder {
+    font-family: inherit;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: #002352;
+    padding: 0 0 0 0.2rem;
+  }
 
-@media ${device.tablet} { 
+  @media ${device.tablet} {
     max-width: 500px;
   }
 
-  @media ${device.laptop} { 
+  @media ${device.laptop} {
     max-width: 565px;
   }
 
@@ -120,33 +118,33 @@ background-color: #C5C5C5;
 `;
 
 export const FormTextarea = styled.textarea`
-background-color: #C5C5C5;
-height: 8rem;
-width: 79vw;
-border-style: none;
-border-radius: 0.3rem;
+  background-color: #c5c5c5;
+  height: 8rem;
+  width: 79vw;
+  border-style: none;
+  border-radius: 0.3rem;
 
-:: placeholder,
-  :: -webkit-textarea-placeholder {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: .8rem;
-  font-weight: 200;
-  color: #002352;
-  padding: .2rem .1rem 0 .2rem;
-}
-  : -ms-textarea-placeholder {
-  font-family: Arial, Helvetica, sans-serif;  
-  font-size: .8rem;
-  font-weight: 200;
-  color: #002352;
-  padding: 0 0 0 .2rem;
-}
+  ::placeholder,
+  ::-webkit-textarea-placeholder {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: #002352;
+    padding: 0.2rem 0.1rem 0 0.2rem;
+  }
+  :-ms-textarea-placeholder {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: #002352;
+    padding: 0 0 0 0.2rem;
+  }
 
-  @media ${device.tablet} { 
+  @media ${device.tablet} {
     max-width: 500px;
   }
 
-  @media ${device.laptop} { 
+  @media ${device.laptop} {
     max-width: 565px;
   }
 
@@ -156,27 +154,27 @@ border-radius: 0.3rem;
 `;
 
 export const Button = styled.input`
-  background-color: #BB6BD9;
+  background-color: #bb6bd9;
   font-family: helvetica;
-  font-size: .7rem;
+  font-size: 0.7rem;
   font-weight: 200;
   color: white;
   border: none;
-  border-radius: .2rem;
+  border-radius: 0.2rem;
   height: 1.2rem;
-  width: 4.5rem; 
-  `;
+  width: 4.5rem;
+`;
 
 export const PostButton = styled.input`
-  background-color: #BB6BD9;
+  background-color: #bb6bd9;
   font-family: helvetica;
-  font-size: .7rem;
+  font-size: 0.7rem;
   font-weight: 200;
   color: white;
   border: none;
-  border-radius: .2rem;
+  border-radius: 0.2rem;
   height: 1.2rem;
-  width: 4.5rem; 
+  width: 4.5rem;
 `;
 
 export const ButtonContainer = styled.div`
@@ -184,80 +182,80 @@ export const ButtonContainer = styled.div`
   justify-content: space-between;
   margin-top: 1rem;
   margin-bottom: 1.5rem;
-`
+`;
 
 export const CloseButton = styled(Link)`
   font-family: helvetica;
-  font-size: .8rem;
+  font-size: 0.8rem;
   font-weight: 200;
   text-decoration: none;
   text-align: right;
-  margin: .6rem 0 0 0;
-  color: rgb(63, 63, 63)
-`
+  margin: 0.6rem 0 0 0;
+  color: rgb(63, 63, 63);
+`;
 //FileUpload styles
 
 export const AddImageButton = styled.input`
-  background-color: #BB6BD9;
+  background-color: #bb6bd9;
   font-family: helvetica;
-  font-size: .7rem;
+  font-size: 0.7rem;
   font-weight: 200;
   color: white;
   border: none;
-  border-radius: .2rem;
+  border-radius: 0.2rem;
   height: 1.2rem;
-  width: 4.5rem;  
+  width: 4.5rem;
 `;
 
 export const FileUploadLabel = styled.label`
-display: block;
-text-align: left;
-font-family: helvetica;
-font-size: .7rem;
-font-weight: 200;
-color: rgb(63, 63, 63);
-margin: .6rem 0 .2rem 0;
+  display: block;
+  text-align: left;
+  font-family: helvetica;
+  font-size: 0.7rem;
+  font-weight: 200;
+  color: rgb(63, 63, 63);
+  margin: 0.6rem 0 0.2rem 0;
 `;
 
 export const FileStatus = styled.p`
-display: block;
-text-align: left;
-font-family: helvetica;
-font-size: .8rem;
-font-weight: 200;
-color: rgb(63, 63, 63);
-margin: .6rem 0 .2rem 0;
-`; 
+  display: block;
+  text-align: left;
+  font-family: helvetica;
+  font-size: 0.8rem;
+  font-weight: 200;
+  color: rgb(63, 63, 63);
+  margin: 0.6rem 0 0.2rem 0;
+`;
 
 export const FileUploadInput = styled.input`
-/* background-color: #C5C5C5; */
-height: 1.3rem;
-width: 80vw;
-border-style: none;
-border-radius: 0.3rem;
-margin: 0 0 10px 0;
+  /* background-color: #C5C5C5; */
+  height: 1.3rem;
+  width: 80vw;
+  border-style: none;
+  border-radius: 0.3rem;
+  margin: 0 0 10px 0;
 
-:: placeholder,
-:: -webkit-input-placeholder {
-  font-family: inherit;
-  font-size: .8rem;
-  font-weight: 200;
-  color: white;
-  padding: 0 0 0 .2rem;
-}
-: -ms-input-placeholder {
-  font-family: inherit;
-  font-size: .8rem;
-  font-weight: 200;
-  color: white;
-  padding: 0 0 0 .2rem;
-}
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    font-family: inherit;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: white;
+    padding: 0 0 0 0.2rem;
+  }
+  :-ms-input-placeholder {
+    font-family: inherit;
+    font-size: 0.8rem;
+    font-weight: 200;
+    color: white;
+    padding: 0 0 0 0.2rem;
+  }
 
-@media ${device.tablet} { 
+  @media ${device.tablet} {
     max-width: 500px;
   }
 
-  @media ${device.laptop} { 
+  @media ${device.laptop} {
     max-width: 565px;
   }
 
