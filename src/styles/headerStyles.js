3de9beae7a602d0159273d725854edb019a89a24
“Steve <stevedunnad@gmail.com>
2020-09-29 15:34:29 +0100

    styles header

--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -8,7 +8,7 @@ import {
 
 //centres the control panel
 
-export const OuterContainer = styled.body` 
+export const OuterContainer = styled.div`
   height: 65px;
   width: 100vw;
   display: flex;
@@ -16,8 +16,9 @@ export const OuterContainer = styled.body`
 `;
 
 //the control panel containing the controls
+
 export const Container = styled.header ` display: block;
-  width: 65vw;
+  width: 90vw;
   height: 45px;
   padding: 17px;
   align-items: center;
@@ -25,18 +26,30 @@ export const Container = styled.header ` display: block;
   border-radius: 3px;
   margin-top: 30px;
 
+  @media ${device.tablet} { 
+    max-width: 498px;
+  }
+
   @media ${device.laptop} { 
+    max-width: 568px;
+  }
+
+  @media ${device.desktop} {
+    max-width: 600px;
+  }
+
+  /* @media ${device.laptop} { 
     max-width: 565px;
   }
 
   @media ${device.desktop} {
     max-width: 565px;
-  }
+  } */
 `;
 
 export const InnerContainer = styled.div` 
-  /* width: 49vw; */
   height: 45px;
+  /* width: 60vw; */
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -55,16 +68,16 @@ export const InnerContainer = styled.div`
 `;
 
 //first flexbox contains login button
-export const LoginContainer = styled.div ` 
+export const LoginContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
 
 //the login button
-export const Button = styled(Link)` 
-  background-color: #BB6BD9;
+export const Button = styled(Link)`
+  background-color: #bb6bd9;
   font-family: helvetica;
-  font-size: .8rem;
+  font-size: 0.8rem;
   font-weight: 200;
   color: white;
   border: none;
@@ -80,18 +93,18 @@ export const Button = styled(Link)`
 `;
 
 //this contains the logo
-export const LogoContainer = styled.div ` 
+export const LogoContainer = styled.div`
   display: flex;
   justify-content: start;
   height: 45px;
 `;
 
 //the travel buddy logo
-export const LogoType = styled.h1 ` 
-  font-family: "Coiny",cursive;
+export const LogoType = styled.h1`
+  font-family: "Coiny", cursive;
   font-size: 30px;
   font-weight: 200;
-  line-height: 25px; 
+  line-height: 25px;
   margin: 0;
   padding: 0;
   width: 100px;
@@ -99,74 +112,66 @@ export const LogoType = styled.h1 `
 `;
 
 //this sub-contains the login, info and signup link
+
 export const LoginInfoSignupContainer = styled.div ` 
 display: flex;
 flex-direction: column;
 justify-content: space-between;
 align-content: space-between;
-width: 55px;
+width:55px;
 height: 50px;
 `;
 
 //this sub-contains the info and signup link
-export const InfoSignupContainer = styled.div ` display: flex;
-justify-content: space-between;
-align-items: center;
-width: 55px;
-height: 22px;
+export const InfoSignupContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 55px;
+  height: 22px;
 `;
 
 //info button
-export const IButton = styled.button` 
-background-color: lightgray;
-border-radius: 50%;
-background-color: #C5C5C5;
-font-family: 'Times New Roman',
-Times,
-serif;
-font-size: .8rem;
-font-weight: 800;
-color: #002352;
-border: none;
-height: 1.3rem;
-line-height: 1rem;
-width: 1.3rem;
-text-decoration: none;
+export const IButton = styled.button`
+  background-color: lightgray;
+  border-radius: 50%;
+  background-color: #c5c5c5;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 0.8rem;
+  font-weight: 800;
+  color: #002352;
+  border: none;
+  height: 1.3rem;
+  line-height: 1rem;
+  width: 1.3rem;
+  text-decoration: none;
 
-:hover {
-background-color: black;
-}
+  :hover {
+    background-color: black;
+  }
 `;
 
 //infopopup container
-export const InfoContainer = styled.div ` 
- width: 20 rem;
- height: 20 rem;
- display: flex;
- align-items: center;
- justify-content: center;
-background-color: darkgrey;
+export const InfoContainer = styled.div`
+  width: 20 rem;
+  height: 20 rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: darkgrey;
 `;
 
-export const InfoTextContainer = styled.div ` 
- 
-`;
+export const InfoTextContainer = styled.div``;
 
 //signup link
-export const SignUpTitle = styled.h1 `
-/* justify-self: flex-start; */
-/* align-self: center; */
-
+export const SignUpTitle = styled.h1`
+  /* justify-self: flex-start; */
+  /* align-self: center; */
 `;
-export const SignUpLink = styled(Link)` 
+export const SignUpLink = styled(Link)`
   font-family: helvetica;
-  font-size: .6rem;
+  font-size: 0.6rem;
   justify-self: flex-start;
   align-self: center;
-  color: #BB6BD9;
+  color: #bb6bd9;
 `;
-
-
-
-
-
